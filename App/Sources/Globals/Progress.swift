1ebf9aefa4f018c6738b43c48f15fa217893ef05
Cihat Gündüz <commits@cihatguenduez.de>
2018-06-22 14:44:26 +0200

    Change organization name to St. Leonhard Gymnasium

--- App/Sources/Globals/Progress.swift
+++ App/Sources/Globals/Progress.swift
@@ -1,6 +1,6 @@
 //
 //  Created by Cihat Gündüz on 26.05.17.
-//  Copyright © 2017 Jamit Labs GmbH. All rights reserved.
+//  Copyright © 2017 St. Leonhard Gymnasium. All rights reserved.
 //
 
 import HandyUIKit
